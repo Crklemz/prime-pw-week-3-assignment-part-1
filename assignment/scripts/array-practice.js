345df5d6e87d2bff8fcb0deaec2887c7d65347d6
Chris Klemz <crklemz@gmail.com>
2021-04-13 12:35:38 -0500

    assignment complete

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -69,7 +69,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favoriteFoods.unshift('Steak');
+console.log(`Added a food item to the beginning: ${favoriteFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -78,3 +79,6 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+removedFood = favoriteFoods.shift();
+console.log('Removed the first food', removedFood);
+console.log('The favorite foods are now', favoriteFoods);
